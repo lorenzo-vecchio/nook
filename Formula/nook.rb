0bbabc1a0d15eef4f839b22bd7cb2e5494a0c1a7
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2026-06-17 23:49:51 +0200

    ci: add goreleaser config, package manifests, CI/CD workflows

---
+++ Formula/nook.rb
@@ -0,0 +1,33 @@
+class Nook < Formula
+  desc "Workspace organizer CLI for developers"
+  homepage "https://github.com/anomalyco/nook"
+  version "0.1.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/anomalyco/nook/releases/download/v0.1.0/nook-darwin-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/anomalyco/nook/releases/download/v0.1.0/nook-darwin-amd64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/anomalyco/nook/releases/download/v0.1.0/nook-linux-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/anomalyco/nook/releases/download/v0.1.0/nook-linux-amd64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "nook"
+  end
+
+  test do
+    system "#{bin}/nook", "--version"
+  end
+end
